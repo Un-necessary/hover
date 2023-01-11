9ef204fc9c5bf3bb764df2e42913f94ce3e9579d
Un-necessary <kcnitesssh7@gmail.com>
2023-01-11 14:32:34 +0545

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+//console.log('check')
+
+const firstElement= document.querySelectorAll(".panel");
+firstElement.forEach((i) => {
+i.addEventListener("mouseover",() => {
+i.classList.add("active");
+});
+
+i.addEventListener("mouseout", ()=> {
+i.classList.remove("active");
+ });
+});
+console.log(firstElement);
